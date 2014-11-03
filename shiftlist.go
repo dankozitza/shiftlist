deb869121f4ec0348ef37cad6bb37dca6444d8aa
Dan Kozitza <dankoz@gmx.us>
2014-11-03 17:30:09 -0600

    cleanup

--- shiftlist.go
+++ shiftlist.go
@@ -1,31 +1,26 @@
 package shiftlist
 
-import (
-	"fmt"
-	"github.com/dankozitza/seestack"
-)
-
 type ShiftList struct {
-	truelist []interface{}
-	MaxIndex int
+	truelist   []interface{}
+	MaxIndex   int
 	NumEntries int
-	oldest int
-	newest int
-	Full bool
+	oldest     int
+	newest     int
+	Full       bool
 }
 
 func New(max int) *ShiftList {
-	if (max <= 0) {
+	if max <= 0 {
 		max = 100
 	}
-	return &ShiftList{make([]interface{}, max + 1), max, 0, 0, -1, false}
+	return &ShiftList{make([]interface{}, max+1), max, 0, 0, -1, false}
 }
 
 func (shl *ShiftList) Add(val interface{}) {
 
 	shl.newest++
 
-	if (shl.NumEntries <= shl.MaxIndex) {
+	if shl.NumEntries <= shl.MaxIndex {
 		shl.NumEntries++
 	}
 
@@ -33,19 +28,14 @@ func (shl *ShiftList) Add(val interface{}) {
 		shl.newest = 0
 		shl.Full = true
 	}
-	if (shl.Full) {
+	if shl.Full {
 		shl.oldest += 1
 		if shl.oldest > shl.MaxIndex {
 			shl.oldest = 0
 		}
 	}
 
-	//fmt.Println(seestack.Short(),
-		//"before shl:", shl)
 	shl.truelist[shl.newest] = val
-	//fmt.Println(seestack.Short(),
-		//"after shl:", shl)
-
 }
 
 func (shl *ShiftList) Get(i int) interface{} {
@@ -57,11 +47,11 @@ func (shl *ShiftList) shiftindex(i int) int {
 
 	n = i + shl.oldest
 
-	if (n > shl.MaxIndex) {
+	if n > shl.MaxIndex {
 		n = n - shl.MaxIndex - 1
 	}
 
-	if (n > shl.MaxIndex || 0 > shl.MaxIndex) {
+	if n > shl.MaxIndex || 0 > shl.MaxIndex {
 		panic("ShiftList: error index out of range!: " + string(n))
 	}
 
